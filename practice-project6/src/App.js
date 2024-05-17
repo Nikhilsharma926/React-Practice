3864c786115196552299c418aca98e90594a9a11
Nikhilsharam962 <niksharma787683@gmail.com>
2024-05-17 11:06:53 +0530

    stlying

---
+++ practice-project6/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'; import './App.css';
+ function App()
+  { 
+    return ( <div className="App">
+      
+       <header className="App-header"> 
+ <img src={logo} className="App-logo" alt="logo" />
+  <p> jai shree ram </p>
+
+  <button> submit</button>
+
+   <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > </a> 
+   </header> </div> ); } 
+   
+   export default App;
