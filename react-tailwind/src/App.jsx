6c57bac8cc46c3e61290da62b3c95357e06e10b8
Nikhilsharam962 <niksharma787683@gmail.com>
2024-06-03 11:40:28 +0530

    modify the logic page

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import products from './products';
-import text from './text';
+
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-700 p-4'>
-      <div className='w-44 h-32 rounded-xl bg-red-600 p-3 text-white' > 
-      <h1>userid </h1>
+    <div className='w-full h-screen bg-gray-800 p-44 '>
+      <div className='w-80 h-16 rounded-xl bg-red-600 p-4  text-white ' > 
+      <h1 >Your Email</h1>
+      <div className='w-80 h-16 rounded-xl bg-red-600  text-white '>
+        <p>Your Password </p>
+
+      </div>
       </div>
       
-    <products/> 
+      
+    
 
     </div>
     
